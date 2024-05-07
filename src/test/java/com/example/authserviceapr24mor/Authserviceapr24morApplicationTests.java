500038e7010b5f352ec294076bca32f34f014ba1
deena <deenaantonym@gmail.com>
2024-05-07 17:59:37 +0530

    Initial commit

---
+++ src/test/java/com/example/authserviceapr24mor/Authserviceapr24morApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authserviceapr24mor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Authserviceapr24morApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
